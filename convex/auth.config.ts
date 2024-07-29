8f03dee643a5d753b5e03816c68b265ee740ad1d
Thai Ly <thai.ly@masterisehomes.com>
2024-07-29 09:16:08 +0700

    update

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://relative-roughy-30.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
---
+++ providers/convex-client-provider.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import {Loading} from '@/components/auth/loading';
+import {ClerkLoading, ClerkProvider, SignedIn, SignedOut, SignInButton, SignOutButton, useAuth} from '@clerk/nextjs';
+import {ConvexReactClient} from 'convex/react';
+import {ConvexProviderWithClerk} from 'convex/react-clerk';
+
+interface ConvexClientProviderProps {
+  children: React.ReactNode;
+}
+
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
+// const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+const convex = new ConvexReactClient(convexUrl);
+
+export const ConvexClientProvider = ({
+  children
+}: ConvexClientProviderProps) => {
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        <SignedIn>
+          {children}
+          <SignOutButton />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <ClerkLoading>
+          <Loading />
+        </ClerkLoading>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
